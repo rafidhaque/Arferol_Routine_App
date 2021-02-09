3d6e02838093eb4d4366bf13aa175f6b5d67e38d
JAZZHAND1 <47551224+JAZZHAND1@users.noreply.github.com>
2021-02-10 03:04:41 +0600

    Firebase installed

--- arferol/App.js
+++ arferol/App.js
@@ -4,6 +4,18 @@ import { StyleSheet, Text, View } from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import AuthStackScreen from './navigation/authstack';
 import SignUp from './screens/SignUp'
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAEF7D1zGr03lrJ6suMUQwwjp5ZG5grQWQ",
+  authDomain: "iutroutineapp.firebaseapp.com",
+  projectId: "iutroutineapp",
+  storageBucket: "iutroutineapp.appspot.com",
+  messagingSenderId: "766237182545",
+  appId: "1:766237182545:web:04b7d6fc6a9c85daf1c670"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 export default function App() {
   return (
