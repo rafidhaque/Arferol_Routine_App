23aba7778321c102771f27ae46ed911ccd0bf3f2
JAZZHAND1 <47551224+JAZZHAND1@users.noreply.github.com>
2021-02-13 00:37:50 +0600

    ScollView for classlist fixed

--- arferol/screens/classlist.js
+++ arferol/screens/classlist.js
@@ -2,33 +2,27 @@ import React from 'react';
 import {View,Text,StyleSheet, ScrollView} from 'react-native';
 import Header from '../components/header';
 import Classdetail from '../components/classdetail';
-
 const Classlist =(props)=>{
-   console.log(props);
+    
     return(
-        <View styles={styles.mainview}> 
+        <View style={styles.mainview}>
              <Header toggledrawer={()=>{
                 props.navigation.toggleDrawer();
             }}
-            title="Classes"
+              title="Classlist"
             >
          </Header>
-         <Text style={styles.textstyle}>
-          Classlist:
-         </Text>  
-         <ScrollView style={{height:600}} >
-            <Classdetail> </Classdetail>
-            <Classdetail> </Classdetail>
-            <Classdetail> </Classdetail>
-            <Classdetail> </Classdetail>
-            <Classdetail> </Classdetail>
-            <Classdetail> </Classdetail>
-            <Classdetail> </Classdetail>
-          </ScrollView>
-
+         <Text style={styles.textstyle}>Classlist:</Text>  
+      <ScrollView style={styles.coursetyle}>
+            <Classdetail></Classdetail>
+            <Classdetail></Classdetail>
+            <Classdetail></Classdetail>
+            <Classdetail></Classdetail>
+            <Classdetail></Classdetail>
+      </ScrollView>
+      
         </View>
     )
-
 }
 
 const styles = StyleSheet.create({
@@ -36,9 +30,9 @@ const styles = StyleSheet.create({
     mainview:{
       flex:1,  
     },
-    courseview:{
+    coursetyle:{
       flex:1,
-      flexDirection:"column" 
+      flexDirection:"column"
     },
     textstyle:{
       fontSize:30,
@@ -48,5 +42,6 @@ const styles = StyleSheet.create({
    
 
 });
+
 
 export default Classlist;
