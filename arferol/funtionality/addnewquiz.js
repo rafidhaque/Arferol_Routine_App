a6f274370ba827655a2662ee2151864b51d91ab3
JAZZHAND1 <47551224+JAZZHAND1@users.noreply.github.com>
2021-02-13 18:03:24 +0600

    Styling changes in ADD QUIZ

--- arferol/funtionality/addnewquiz.js
+++ arferol/funtionality/addnewquiz.js
@@ -1,9 +1,10 @@
 import React,{useState} from 'react';
 import {View,Text,StyleSheet,TextInput, Button} from 'react-native';
-import Header from '../components/header';
+import Header from '../components/header'
+import Calendar from 'react-native-calendars';
 
 const Addnewquiz =(props)=>{
-      const [date,setdate] = useState("");
+      const [date,setdate] = useState(new Date());
       const [syllabus,setsyllabus] =  useState("");
 
       return(
@@ -15,27 +16,42 @@ const Addnewquiz =(props)=>{
               >
            </Header>
            <Text style={styles.textstyle}>Add:</Text>  
-           <View style={styles.viewstyle}>  
+           <View style={styles.view}>  
             <TextInput
-                 style={{ height: 40, borderColor: 'dodgerblue', borderWidth: 2 }}
+                 style={{ height: 40, borderColor: 'dodgerblue', borderWidth: 2 ,width:100,paddingLeft:33}}
                  onChangeText={function(currentvalue){
 
                  }}
                  placeholder="Date"
-           /> 
+           />
+           <TextInput
+                 style={{ height: 40, borderColor: 'dodgerblue', borderWidth: 2,width:100,paddingLeft:28 }}
+                 onChangeText={function(currentvalue){
+
+                 }}
+                 placeholder="Month"
+           />
+            <TextInput
+                 style={{ height: 40, borderColor: 'dodgerblue', borderWidth: 2,width:152,paddingLeft:60 }}
+                 onChangeText={function(currentvalue){
+
+                 }}
+                 placeholder="Year"
+           />
+
            </View>
 
 
-           <View style={styles.viewstyle}>  
+           <View style={styles.syllbutton}>  
             <TextInput
-                 style={{ height: 40, borderColor: 'dodgerblue', borderWidth: 2 }}
+                 style={{ height: 40, borderColor: 'dodgerblue', borderWidth: 2,paddingLeft:10}}
                  onChangeText={function(currentvalue){
 
                  }}
                  placeholder="Syllabus"
            /> 
            </View>
-           <View style={styles.viewstyle}> 
+           <View style={styles.syllbutton}> 
                <Button title="Confirm"></Button>
           </View>
           </View>
@@ -57,7 +73,19 @@ const Addnewquiz =(props)=>{
         marginLeft:10
       },
       viewstyle:{
-          padding:10
+          padding:0,
+          flexDirection:"column"
+      },
+      view:{
+        flexDirection:"row",
+        marginLeft:10,
+        marginEnd:8,
+        justifyContent:"space-between"
+      },
+      syllbutton:{
+        marginTop:8,
+        marginLeft:10,
+        marginEnd:8
       }
      
   
